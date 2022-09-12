b542bef463a6267712e4f67329da79534ae6f3c1
Lev Epshtein <lev.epstein@gmail.com>
2022-09-12 20:32:53 +0300

    Update nodejs_docker.groovy

--- basics/dsl/nodejs_docker.groovy
+++ basics/dsl/nodejs_docker.groovy
@@ -1,6 +1,9 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/levep/docker-cicd.git','master') 
+        git('https://github.com/levep/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL lev')
+            node / gitConfigEmail('jenkins-dsl@lev.academy')
+        } 
     }
     triggers {
         scm('H/5 * * * *')
